a3ba43992090b26a1563d8d6295826198ca89f18
Antoine Charriere <antoine.charriere.16@eigsi.fr>
2023-09-19 10:31:20 +0200

    améliorations

--- src/components/modal.js
+++ src/components/modal.js
@@ -74,9 +74,7 @@ function firstNameControl() {
   const formDataFirstName = document.querySelector(".formData:first-child");
   return controlvisual(regExFirstLastName(firstName),formDataFirstName,"Ce champ doit contenir au minimum 2 caractères")}
 firstNameInput.addEventListener("input", function () {
-  const firstName = firstNameInput.value;
-  const formDataFirstName = document.querySelector(".formData:first-child");
-  controlvisual(regExFirstLastName(firstName),formDataFirstName,"Ce champ doit contenir au minimum 2 caractères")
+  firstNameControl()
 })
 
 /**
@@ -88,9 +86,7 @@ function lastNameControl() {
   return controlvisual(regExFirstLastName(lastName),formDataLastName,"Ce champ doit contenir au minimum 2 caractères")
 }
 lastNameInput.addEventListener("input", function () {
-  const lastName = lastNameInput.value;
-  const formDataLastName = document.querySelector(".formData:nth-child(2)");
-  controlvisual(regExFirstLastName(lastName),formDataLastName,"Ce champ doit contenir au minimum 2 caractères")
+  lastNameControl()
 })
 /**
  * Contrôle du mail dans le formulaire pour respecter le format d'un mail et retourne true si c'est respecté ou false.
@@ -101,9 +97,7 @@ function emailControl() {
   return controlvisual(regExmail(email),formDataemail,"Email invalide")
 }
 emailInput.addEventListener("input", function () {
-  const email = emailInput.value;
-  const formDataemail = document.querySelector(".formData:nth-child(3)")
-  controlvisual(regExmail(email),formDataemail,"Email invalide")
+  emailControl()
 })
 /**
  * Contrôle de la date de naissance dans le formulaire toutes les dates sont acceptés sauf celle d'aujourd'hui et retourne true si c'est respecté ou false.
@@ -116,11 +110,7 @@ function birthdateControl(){
   return controlvisual(birthdate < today,formDatabirthdate,"Vous devez entrer votre date de naissance")
 }
 birthdateInput.addEventListener("input", function() {
-  const birthdateStr = birthdateInput.value;
-  const birthdate = new Date(birthdateStr);
-  const today = new Date().setHours(0,0,0,0)
-  const formDatabirthdate = document.querySelector(".formData:nth-child(4)");
-  controlvisual(birthdate < today,formDatabirthdate,"Vous devez entrer votre date de naissance")
+  birthdateControl()
 })
 /**
  * Contrôle du nombre de tournoi dans le formulaire avec une saisie de chiffre et retourne true si c'est respecté ou false.
@@ -132,10 +122,7 @@ function tournamentControl() {
     return controlvisual(isNumeric,formDatatournament,"Vous devez entrer un nombre")
 }
   tournamentInput.addEventListener("input", function() {
-    const quantitytournament = tournamentInput.value
-    const isNumeric = /^\d+$/.test(quantitytournament);
-    const formDatatournament = document.querySelector(".formData:nth-child(5)");
-    controlvisual(isNumeric,formDatatournament,"Vous devez entrer un nombre")    
+    tournamentControl()  
   })
 
 
@@ -175,8 +162,7 @@ function cguControl() {
   
 }
   cguInput.addEventListener("input", function() {
-  const formDatacgu = document.querySelector(".formData:nth-child(8)");
-    controlvisual(cguInput.checked,formDatacgu,"Avez-vous lu les conditions générales d'utilisation ?")
+    cguControl()
 })
 /**
  * Bouton qui envoie vers une nouvelle page si toutes les conditions précédentes sont respectés
